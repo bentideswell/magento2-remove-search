a9ee985d7e3e76d2f9c04a22a8b404f5a0dc2121
Ben Tideswell <ben@fishpig.co.uk>
2023-04-26 13:57:46 +0100

    1.0.0

---
+++ Setup/Model/SearchConfig.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace FishPig\RemoveSearch\Setup\Model;
+
+class SearchConfig extends \Magento\Setup\Model\SearchConfig
+{
+    /**
+     *
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     *
+     */
+    public function saveConfiguration(array $inputOptions)
+    {
+    }
+
+    /**
+     *
+     */
+    public function validateSearchEngine()
+    {
+    }
+}
---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @url FishPig.com
+ */
+declare(strict_types=1);
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'FishPig_RemoveSearch', __DIR__);
