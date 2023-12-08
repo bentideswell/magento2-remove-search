bbb4289804bc753ed7bff8045694e58c3e511530
Ben Tideswell <ben@fishpig.co.uk>
2023-12-08 11:38:44 +0000

    1.0.9

---
+++ Mock/Magento/Search/Model/SearchEngine/Validator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ */
+declare(strict_types=1);
+
+namespace Magento\Search\Model\SearchEngine;
+
+class Validator implements ValidatorInterface
+{
+    /**
+     *
+     */
+    public function validate(): array
+    {
+        return [];
+    }
+}
---
+++ Mock/Magento/Search/Setup/CompositeInstallConfig.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ */
+declare(strict_types=1);
+
+namespace Magento\Search\Setup;
+
+class CompositeInstallConfig implements InstallConfigInterface
+{
+    /**
+     *
+     */
+    public function configure(array $inputOptions)
+    {
+    }
+}
